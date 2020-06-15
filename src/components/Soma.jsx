ad3a3ce7583d1837579df88f8dee67f7e1b8270e
gabriel.bucalon <bucalongabriell@gmail.com>
2020-06-15 13:18:59 -0300

    create components sum, average and draw

---
+++ src/components/Soma.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Card from './Card';
+
+export default props => {
+    return (
+        <Card title="Soma dos números" blue>
+            <div>
+                <span>
+                    <span>Resultado : </span>
+                    <strong>5</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
