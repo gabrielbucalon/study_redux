bd2469a0684fa8ea123c0b9a59f07b5be6fb7a0d
gabriel.bucalon <bucalongabriell@gmail.com>
2020-06-15 11:51:15 -0300

    adjust component card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,26 @@
 import './Card.css';
 import React from 'react';
 
+function getColor(props) {
+    if (props.red) return "Red";
+    if (props.green) return "Green";
+    if (props.blue) return "Blue";
+    if (props.purple) return "Purple";
+
+}
+
 export default props => {
     return (
-        <div>
-            Card!
+        <div className={`Card ${getColor(props)}`}>
+            <div className="Header">
+                <span className="title">
+                    {props.title}
+                </span>
+            </div>
+
+            <div className="Content">
+                {props.children}
+            </div>
         </div>
     )
 }
